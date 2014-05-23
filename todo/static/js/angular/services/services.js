cf1f195b5e8c4847dab18fbb3df051944ae74063
Arthur Alvim <arth.alvim@gmail.com>
2014-05-23 18:22:45 -0300

    Updating angular scripts and adding $resource .

--- todo/static/js/angular/services/services.js
+++ todo/static/js/angular/services/services.js
@@ -2,19 +2,21 @@
 
     var services = angular.module('todo.services', ['ngResource']);
 
+    var baseUrl = 'http://localhost\\:8000';
+
     services.factory('TodoListsFactory', function ($resource) {
-        return $resource('/api/todo/', {}, {
-            query: { method: 'GET', isArray: true },
-            create: { method: 'POST' }
-        })
+        return $resource(baseUrl+'/api/todo/', {}, {
+            querya: { method: 'GET', isArray: true },
+            createa: { method: 'POST'}
+        });
     });
 
     services.factory('TodoListFactory', function ($resource) {
-        return $resource('/api/todo/:id', {}, {
-            show: { method: 'GET' },
+        return $resource(baseUrl+'/api/todo/:id', {}, {
+            showa: { method: 'GET' },
             update: { method: 'PUT', params: {id: '@id'} },
             delete: { method: 'DELETE', params: {id: '@id'} }
-        })
+        });
     });
 
 })();
--- todo/static/js/angular/todo.js
+++ todo/static/js/angular/todo.js
@@ -1,41 +1,44 @@
 (function(){
-    var app = angular.module('todo', []);
+    var app = angular.module('todo', ['todo.services']);
 
     app.config(function($interpolateProvider) {
         $interpolateProvider.startSymbol('{_');
         $interpolateProvider.endSymbol('_}');
     });
 
-    app.directive('todoList', [ '$http', '$location', 'TodoListsFactory',
-                'TodoListFactory',
-                function($http, $location, TodoListsFactory, TodoListFactory){
+    app.directive('todoList', [ '$http', '$location', 'TodoListsFactory', 'TodoListFactory', function($scope, $http, $location, TodoListsFactory, TodoListFactory){
+
         return {
             restrict: 'E',
             templateUrl: 'static/js/angular/views/todo-list.html',
-            controller: function($http, $location, TodoListsFactory,
-                                 TodoListFactory){
+            controller: function($http, $location){
 
                 var todo = this;
                 todo.todolist = [];
+                todo.new_todo = {};
 
-                /* callback for ng-click 'detail': */
-                $scope.detail = function (id) {
+                todo.detail = function (id) {
                   $location.path('/api/todo/' + id);
                 };
 
-                /* callback for ng-click 'delete': */
-                $scope.delete = function (id) {
+                todo.create = function () {
+                    debugger;
+                  operation = TodoListsFactory.create(todo.new_todo);
+                  todo.new_todo = {}
+                  todo.todolist = TodoListsFactory.query();
+                };
+
+                todo.delete = function (id) {
                   TodoListFactory.delete({ id: id });
-                  $scope.todolist = TodoListsFactory.query();
+                  todo.todolist = TodoListsFactory.query();
                 };
 
-                /* callback for ng-click 'update': */
-                $scope.update = function (todo_obj) {
-                  TodoListFactory.update(todo_obj);
-                  $scope.todolist = TodoListsFactory.query();
+                todo.update = function () {
+                  TodoListFactory.update(todo.new_todo);
+                  todo.todolist = TodoListsFactory.query();
                 };
 
-                $scope.todolist = TodoListsFactory.query();
+                todo.todolist = TodoListsFactory.query();
 
             },
             controllerAs: 'todoCtrl'
@@ -43,5 +46,3 @@
     }]);
 
 })();
-
-
